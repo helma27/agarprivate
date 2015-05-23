3381d716522fc43bafecaca200413fd4d33bde96
electronoob <me@electronoob.com>
2015-05-23 06:40:17 +0200

    deathstar

--- mods.js
+++ mods.js
@@ -49,7 +49,7 @@ function agariomodsRuntimeInjection() {
 }
 function agariomodsRuntimePatches() {
 	//s
-	var ourskins = "711;agariomods;bobross;bobsaget;chrome;controless;doggie;dong;electronoob;facebook;fishies;fuckfacebook;github;hitler;hydro;instagram;isis;itchey;itchyfeetleech;klingon;knicks;love;nazi;orange;pinkfloyd;piratebay;quantum leap;rhcp;rockets;rolfharris;slack;snapchat;space;spawn;spurs;uppercase;voyager;white light;youtube";
+	var ourskins = "711;deathstar;agariomods;bobross;bobsaget;chrome;controless;doggie;dong;electronoob;facebook;fishies;fuckfacebook;github;hitler;hydro;instagram;isis;itchey;itchyfeetleech;klingon;knicks;love;nazi;orange;pinkfloyd;piratebay;quantum leap;rhcp;rockets;rolfharris;slack;snapchat;space;spawn;spurs;uppercase;voyager;white light;youtube";
 	gamejs = gamejs.replace(';reddit;',';reddit;'+ourskins+';');
 	gamejs = gamejs.replace(W + '[b]=new Image,'+W+'[b].src="skins/"+b+".png"',W +'[b]=new Image,'+W+'[b].crossOrigin = "Anonymous",'+W+'[b].src="skins/"+b+".png"');
 	gamejs = gamejs.replace('b=this.name.toLowerCase();', 'b=this.name.toLowerCase();var agariomods="";var ourskins = "'+ourskins+'";if((b.length >0) && (ourskins.indexOf(b)>-1)) {agariomods="http://skins.agariomods.com/i/"+b+".png";} else if (b.substring(0, 2) == "i/") {agariomods="http://i.imgur.com/"+this.name.substring(2)+".jpg";} else {agariomods="http://agar.io/skins/" + this.name.toLowerCase() + ".png";}');
