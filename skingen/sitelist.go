726be9f5ddb39e4fda5fd2f9185ddf963b7118dd
electronoob <me@electronoob.com>
2015-05-30 20:06:21 +0200

    updated skingen script for skins.agariomods.com to fit our new theme

--- skingen/sitelist.go
+++ skingen/sitelist.go
@@ -11,76 +11,24 @@ func main() {
 	fmt.Print(`
 
 
-
-
 <html>
 	<head>
-		<title>AGARIOMODS - SKINS</title>
-		<link rel="shortcut icon" href="http://www.iconj.com/ico/o/x/oxfb9bqt6o.ico" type="image/x-icon" />
-		<meta charset="utf-8">
-  		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<link href='http://fonts.googleapis.com/css?family=Ubuntu:700' rel='stylesheet' type='text/css'>
-  		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-		<!--WEBSITE STYLES-->
-			<style>
-				/* BODY STYLE */
-				body {
-					background: rgba(16, 16, 16, 1);
-					font-family: 'Ubuntu', sans-serif;
-					padding: 10px;
-					margin: 0px;
-					background-image: url("http://i.imgur.com/rgXlmkc.png");
-				}
-				/* CONTENT STYLE */
-				main {
-					background: rgba(0, 0, 0, .6);
-					color: rgba(218, 218, 218, 1);
-					font-family: 'Ubuntu', sans-serif;
-					padding: 0px;
-					margin-left: auto;
-					margin-right: auto;
-					margin-bottom: auto;
-					padding-left: 20px;
-					max-width: 1000px;
-				}
-				.navigate{
-					background: linear-gradient(rgba(215, 18, 162, 1),  rgba(76, 0, 153, 1));
-				}
-				#logo {
-					vertical-align: middle;
-					height: 30px;
-				}
-				/* INSTALL BUTTON STYLE */
-				#ScriptButton {
-					background-color: rgba(255, 140, 0, 0.3);
-					border-radius: 25px;
-					border: 5px solid rgba(255, 165, 0, 1);
-					padding: 10px;
-					color: rgba(255, 255, 255, 1);
-					font-weight: 900;
-					text-decoration: none;
-				}
-				#ScriptButton:hover {
-					background-color: rgba(255, 215, 0, 0.7);
-					border-radius: 25px;
-					padding: 10px;
-				}
-				/* TEMPORARY FIX */
-				#information {
-					padding-top: 35px;
-				}
-				/* skins */
-				div.skins { margin-top: 20px; }
-				span.skins { font-size: 3em; margin-left: 30px;}
-			</style>
+		<head>
+			<title>AGARIOMODS - LINKS</title>
+			<link rel="shortcut icon" href="//www.iconj.com/ico/o/x/oxfb9bqt6o.ico" type="image/x-icon" />
+			<meta charset="utf-8">
+			<link rel="stylesheet" type="text/css" href="//agariomods.com/sitestyle.css">
+  			<meta name="viewport" content="width=device-width, initial-scale=1">
+  			<link href='//fonts.googleapis.com/css?family=Ubuntu:700' rel='stylesheet' type='text/css'>
+  			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		</head>
 		<!--DIVISION MARKER: HEAD - BODY-->
 		<body>
 			<!--Navigation Links-->
-			<nav class="navbar navbar-inverse navbar-fixed-top navigate">
-			  <div class="container-fluid">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+			  <div class="container-fluid navigate">
 			    <div class="navbar-header">
 				 <button type="button" class="navbar-toggle navbar-right btn-lg " data-toggle="collapse" data-target="#myNavbar" style="background-color: rgba(128, 128, 128, .75)">
 				   <span class="icon-bar"></span>
@@ -94,14 +42,16 @@ func main() {
 				   <li><a href="http://agariomods.com/index.html">Home</a></li>
 				   <li><a href="http://agariomods.com/mumble.html">Mumble</a></li>
 				   <li><a href="http://agariomods.com/about.html">About</a></li> 
-				   <li><a href="http://agariomods.com/links.html">Links</a></li> 
-				  <li class="active"><a href="http://skins.agariomods.com">Skins</a></li>
+				   <li><a href="http://agariomods.com/links.html">Links</a></li>
+				   <li><a class="active" href="http://skins.agariomods.com">Skins</a></li>
+                                   <li><a href="http://chat.agariomods.com">Chatroom</a></li>
+                                   <li><a href="http://agariomods.com/twitch.html">Twitch</a></li>
 				 </ul>
 			    </div>
 			  </div>
 			</nav>
 			<main id="information">
-				<div id="information">
+			<div id="information">
 
 
 <h1>agariomods.com skin server</h1>
@@ -143,8 +93,8 @@ func main() {
 
 				</div>
 			</main>
-		</body>
-	</html>
+        </body>
+</html>
 `);
 
 }
