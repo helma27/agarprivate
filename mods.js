1945108fda2cc3bf40353f0db7fabc30ab878ab4
electronoob <github@electronoob.com>
2015-05-31 17:50:36 +0200

    rolled out mass-graph and tidied some loose ends

--- mods.js
+++ mods.js
@@ -1,4 +1,4 @@
-var ourskins = "1up;8ball;LLhyy5H;agariomods.com;albania;android;anonymous;apple;atari;awesome;baka;bandaid;bane;baseball;basketball;batman;beats;bender;bert;bitcoin;blobfish;bobross;bobsaget;boo;boogie2988;borg;bp;breakfast;buckballs;burgundy;butters;byzantium;charmander;chechenya;chickfila;chocolate;chrome;cj;coca cola;cokacola;controless;converse;cornella;creeper;cyprus;czechrepublic;deadpool;deal with it;deathstar;derp;dickbutt;doge;doggie;dolan;domo;domokun;dong;donut;dreamcast;ebin;egg;egoraptor;egypt;epicface;expand;eye;facebook;fast forward;fastforward;fbi;fidel;finn;firefox;fishies;flash;florida;freeman;freemason;friesland;frogout;fuckfacebook;gaben;garfield;gaston;getinmybelly;getinthebox;gimper;github;giygas;gnomechild;gonzo;grayhat;halflife;halflife3;halo;handicapped;hap;hatty;hebrew;heisenburg;helix;hipsterwhale;hitler;honeycomb;hydro;iceland;ie;illuminati;imgur;imperial japan;imperialjapan;instagram;isaac;isis;isreal;itchyfeetleech;ivysaur;james bond;java;jew;jewnose;jimmies;kappa;kenny;kingdomoffrance;kingjoffrey;kirby;klingon;knightstemplar;knowyourmeme;kyle;ladle;lenny;lgbt;libertyy;liechtenstien;linux;love;luigi;macedonia;malta;mario;mars;maryland;masterball;mastercheif;mcdonalds;meatboy;meatwad;megamilk;mike tyson;mlg;moldova;mortalkombat;mr burns;mr.bean;mr.popo;n64;nasa;nazi;nick;nickelodeon;nipple;northbrabant;nosmoking;notch;nsa;obey;osu;ouch;pandaexpress;pedo;pedobear;peka;pepe;pepsi;pewdiepie;pi;pig;piggy;pika;pinkfloyd;pinkstylist;piratebay;pizza;playstation;poop;potato;quantum leap;rageface;rewind;rockstar;rolfharris;rss;satan;serbia;shell;shine;shrek;sinistar;sir;skull;skype;skyrim;slack;slovakia;slovenia;slowpoke;smash;snafu;snapchat;soccer;soliare;solomid;somalia;space;spawn;spiderman;spongegar;spore;spy;squirtle;starbucks;starrynight;stitch;stupid;superman;taco;teamfortress;tintin;transformers;triforce;trollface;tubbymcfatfuck;turkey;twitch;twitter;ukip;uppercase;uruguay;utorrent;voyager;wakawaka;wewlad;white  light;windows;wwf;wykop;yinyang;ylilauta;yourmom;youtube;zoella;zoidberg";
+var ourskins = "1up;8ball;LLhyy5H;agariomods.com;albania;android;anonymous;apple;atari;awesome;baka;bandaid;bane;baseball;basketball;batman;beats;bender;bert;bitcoin;blobfish;bobross;bobsaget;boo;boogie2988;borg;bp;breakfast;buckballs;burgundy;butters;byzantium;charmander;chechenya;chickfila;chocolate;chrome;cj;coca cola;cokacola;controless;converse;cornella;creeper;cyprus;czechrepublic;deadpool;deal with it;deathstar;derp;dickbutt;doge;doggie;dolan;domo;domokun;dong;donut;dreamcast;ebin;egg;egoraptor;egypt;epicface;expand;eye;facebook;fast forward;fastforward;fbi;fidel;finn;firefox;fishies;flash;florida;freeman;freemason;friesland;frogout;fuckfacebook;gaben;garfield;gaston;getinmybelly;getinthebox;gimper;github;giygas;gnomechild;gonzo;grayhat;halflife;halflife3;halo;handicapped;hap;hatty;hebrew;heisenburg;helix;hipsterwhale;hitler;honeycomb;hydro;iceland;ie;illuminati;imgur;imperial japan;imperialjapan;instagram;isaac;isis;isreal;itchyfeetleech;ivysaur;james bond;java;jew;jewnose;jimmies;kappa;kenny;kingdomoffrance;kingjoffrey;kirby;klingon;knightstemplar;knowyourmeme;kyle;ladle;lenny;lgbt;libertyy;liechtenstien;linux;love;luigi;macedonia;malta;mario;mars;maryland;masterball;mastercheif;mcdonalds;meatboy;meatwad;megamilk;mike tyson;mlg;moldova;mortalkombat;mr burns;mr.bean;mr.popo;n64;nasa;nazi;nick;nickelodeon;nipple;northbrabant;nosmoking;notch;nsa;obey;osu;ouch;pandaexpress;pedo;pedobear;peka;pepe;pepsi;pewdiepie;pi;pig;piggy;pika;pinkfloyd;pinkstylist;piratebay;pizza;playstation;poop;potato;quantum leap;rageface;rewind;rockstar;rolfharris;rss;satan;serbia;shell;shine;shrek;sinistar;sir;skull;skype;skyrim;slack;slovakia;slovenia;slowpoke;smash;snafu;snapchat;soccer;soliare;solomid;somalia;space;spawn;spiderman;spongegar;spore;spy;squirtle;starbucks;starrynight;stitch;stupid;superman;taco;teamfortress;tintin;transformers;triforce;trollface;tubbymcfatfuck;turkey;twitch;twitter;ukip;uppercase;uruguay;utorrent;voyager;wakawaka;wewlad;white  light;windows;wwf;wykop;yinyang;ylilauta;yourmom;youtube;zoella;zoidberg;kitty;electrokitty";
 
 var gamejs = "", modBlocking = true;
 var tester = document.getElementsByTagName("script");
@@ -12,6 +12,74 @@ var c3eg2 = '';
 var test = 0;
 var passed = 0;
 var failed = 0;
+
+/* begin folding in mike's code for mass graph */
+var chart_update_interval = 10;
+var chart = null;
+var chart_data = [];
+var chart_counter = 0;
+var chart_s = '';
+var chart_m = '';
+var chart_G = '';
+var chart_Na= '';
+var chart_k = '';
+
+
+$(document).ready(function() {
+    $('body').append('\
+<div id="chart-container" style="position:absolute; height:176px; width:300px; left:10px; bottom:34px"></div>\
+<div id="chart-container-agariomods" style="position:absolute; height:20px; width:300px; left:10px; bottom:200px;">&nbsp;agariomods.com - modding <b>without</b> cheating</div>\
+');
+});
+function ResetChart(){
+    chart = null;
+    chart_data.length = 0;
+    chart_counter = 0;
+    jQuery('#chart-container').empty();     
+}
+function GetRgba(hex_color, opacity){
+    var patt = /^#([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/;
+    var matches = patt.exec(hex_color);
+    return "rgba("+parseInt(matches[1], 16)+","+parseInt(matches[2], 16)+","+parseInt(matches[3], 16)+","+opacity+")";
+}
+function UpdateChart(mass, color)
+{
+    chart_counter++;
+    if (chart_counter%chart_update_interval > 0) return;
+        
+    if (chart === null){
+        chart = new CanvasJS.Chart("chart-container",{
+            interactivityEnabled: false,
+            title: null,
+            axisX:{      
+                valueFormatString: " ",
+                lineThickness: 0,
+                tickLength: 0
+            },
+            axisY:{
+                lineThickness: 0,
+                tickLength: 0,
+                gridThickness: 2,
+                gridColor: "white",
+                labelFontColor: "white"
+            },
+            backgroundColor: "rgba(0,0,0,0.2)",
+            data: [{
+                type: "area",
+                color: color,
+                dataPoints: chart_data 
+            }]
+        });
+    }
+    chart_data.push({
+        x: chart_counter,
+        y: mass
+    });
+    chart.render();
+    // Sorry but the CanvasJS is intrusive to gameplay. Developers will view source anyway.
+    jQuery('.canvasjs-chart-credit').hide();
+}
+
 for (i=0; i<tester.length; i++ ){
 	src = tester[i].src;
 	if (src.substring(0, main_out_url.length ) == main_out_url) {
@@ -31,6 +99,17 @@ if(discovered_mainouturl != 0) {
 		W = gamejs.substr(offset,1);
 		var components = /strokeText\((.{1,14})\);/.exec(gamejs);
 		c3eg2 = components[1];
+		var components = /\((.)\=..x,.\=..y\)/.exec(gamejs);		
+		chart_s = components[1];
+		var components = /\(.\=(.).x,.\=..y\)/.exec(gamejs);
+		chart_m = components[1];
+		var components = /(.)\=Math.max\(.,..\(\)\);/.exec(gamejs);
+		chart_G = components[1];
+		var components = /.\=Math.max\(.,(..)\(\)\);/.exec(gamejs);
+		chart_Na = components[1];
+		var components = /(.)\[0\]\.name\&\&\(/.exec(gamejs);
+		chart_k = components[1];
+		console.log ("chartmod info: chart_m = "+chart_m+";  chart_s = "+chart_s+"; chart_G = "+chart_G+"; chart_Na = "+chart_Na+"; chart_k = "+chart_k);
 		agariomodsRuntimeInjection();
 	});
 }
@@ -47,6 +126,9 @@ function httpGet(theUrl, callback) {
 	};
 }
 function agariomodsRuntimeInjection() {
+	var script = document.createElement("script");
+	script.src='//cdnjs.cloudflare.com/ajax/libs/canvasjs/1.4.1/canvas.min.js';
+        document.head.appendChild(script);
 	var tester = document.getElementsByTagName("head");
 	var oldhtml = tester[0].innerHTML;
 	oldhtml = oldhtml.replace('width:350px;', '');
@@ -68,6 +150,13 @@ function agariomodsRuntimeInjection() {
 	$("#canvas").on('mousedown', function(event){
 		event.preventDefault();
 	});
+        $("#chart-container").on('mousedown', function(event){
+                event.preventDefault();
+        });
+        $("#chart-container-agariomods").on('mousedown', function(event){
+                event.preventDefault();
+        });
+
 }
 function agariomodsRuntimePatches() {
         gamejs = gamejs.replace(';reddit;',';reddit;'+ourskins+';');
@@ -75,6 +164,11 @@ function agariomodsRuntimePatches() {
         gamejs = gamejs.replace(W +'['+b+'].src="skins/"+'+b+'+".png"',W+'['+b+'].src=agariomods');
         gamejs = gamejs.replace("this._stroke&&b.strokeText("+c3eg2+");b.fillText("+c3eg2+")", "if (String(c).substring(0, 2) != \"i/\") {this._stroke&&b.strokeText("+c3eg2+");b.fillText("+c3eg2+")}");
         gamejs = gamejs.replace(b+"=this.name.toLowerCase();", b+"=this.name.toLowerCase(); if ("+b+".substring(0, 2) == \"i/\") {" +Ja+ "+="+b+";} ;");
+	gamejs = gamejs.replace('('+chart_s+'='+chart_m+'.x', '(ResetChart(), '+chart_s+'='+chart_m+'.x');
+	gamejs = gamejs.replace(chart_G+'=Math.max('+chart_G+','+chart_Na+'());', 'var current = '+chart_Na+'();'+chart_G+' = Math.max('+chart_G+', current);');
+	gamejs = gamejs.replace('setValue("Score:','setValue("Current: " + ~~(current / 100) + "  High:');
+	gamejs = gamejs.replace('24-10,b+10,34),','24, 300, 24),');
+	gamejs = gamejs.replace('10-24-5));','10-24-5), ('+chart_k+' && '+chart_k+'[0] && UpdateChart(current/100, GetRgba('+chart_k+'[0].color, 0.4))));');
 	console.log ("Begin regression testing of agariomods.com userscript.");
 	testCondition((-1 != gamejs.indexOf(';reddit;'+ourskins)), test++, "add our skinlist to the original game skinlist.");
 	testCondition((-1 != gamejs.indexOf('var agariomods="";var ourskins'  )), test++, "add check for which skin mode we are in. be it no skin, default skin, imgur skin, or an agariomods skin.");
@@ -82,6 +176,11 @@ function agariomodsRuntimePatches() {
 	testCondition((-1 != gamejs.indexOf( b+"=this.name.toLowerCase(); if ("+b+".substring(0, 2) == \"i/\") {" +Ja+ "+="+b+";} ;"  )), test++, "add imgur check #2.");
 	testCondition((-1 != gamejs.indexOf(W+'['+b+'].src=agariomods')), test++, "add agariomods variable to replace src of image");
 	testCondition((-1 != gamejs.indexOf("if (String(c).substring(0, 2) != \"i/\") {this._stroke")), test++, "add imgur check for hiding username when using imgur id");
+	testCondition((-1 != gamejs.indexOf('(ResetChart(), '+chart_s+'='+chart_m+'.x')), test++, "add mikes call to resetchart");
+	testCondition((-1 != gamejs.indexOf('var current = '+chart_Na+'();'+chart_G+' = Math.max('+chart_G+', current);')),  test++, "add mikes current score buffer");
+	testCondition((-1 != gamejs.indexOf('setValue("Current: " + ~~(current / 100) + "  High:')),  test++, "add mikes current and high score cosmetics");
+	testCondition((-1 != gamejs.indexOf('24, 300, 24),')),  test++, "add mikes resized score rectangle");
+	testCondition((-1 != gamejs.indexOf('10-24-5), ('+chart_k+' && '+chart_k+'[0] && UpdateChart(current/100, GetRgba('+chart_k+'[0].color, 0.4))));')),  test++, "add mikes chart update and colour setting");
 	console.log ("Testing complete, "+passed+" units passed and "+failed+" units failed.");
 }
 function testCondition (condition, id, comment) {
@@ -185,11 +284,11 @@ function agariomodsRuntimeHacks() {
 	window.WebSocket = function(data) {
 		if (modBlocking == true) {
 			newWebSocket = new window.WebSocket_original(data);
-			jQuery('#includedContent').html("v1.8.0<br>electronoob says, <small>\"Thanks to intel and forzero we now have 250 skins to offer all collected from reddit and named appropriately. There will be a few duplicates for now so please excuse those as the vast majority are unique and awesome. \"\
-<br><br>Here is the IP address of the server you are connected to currently, pass it to your friends for team playing.<h1>" + data.replace('ws://', '') + "</h1>&nbsp;\
-                Get IP address from friend.\
-                Put it in text box below.\
-                Press the swirly icon next to it.\
+			jQuery('#includedContent').html("v1.8.1: <font color=\"yellow\">Thanks to intel and forzero for merging in 250 skins from reddit. We must also thank the excellent <a target=\"_blank\" href=\"https://www.reddit.com/r/Agario/comments/37vc98/i_added_a_realtime_graph_of_mass_over_time_i/\">/u/mikeyk730</a>  for allowing us to merge in this brilliant mass-graph mod!</font>\
+<br><br>Here is the IP address of the server you are connected to currently, pass it to your friends for team playing.<h1>" + data.replace('ws://', '') + "</h1>\
+                Get IP from friend.\
+                Put in box below.\
+                Press the swirly icon.\
                 <br><b>Note:</b> Check with your friend to see whos #1 on the leaderboard\
         <div style=\"background-color: #ffffff; color: #000000; padding: 2px; margin: 0px;\">\
                 <small><b>Disable ad blockers</b>&nbsp;- They are breaking the game and our modifications in random and unexpected ways.</small>\
