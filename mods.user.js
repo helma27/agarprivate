d3c72740c7ca6457736ddebda43aad25b21ec699
root <root@vps169161.ovh.net>
2015-06-22 02:03:53 +0200

    rollout preregister

--- mods.user.js
+++ mods.user.js
@@ -1,22 +1,21 @@
 // ==UserScript==
-// @name       AgarioMods Script
-// @namespace	 electronoob-agarmods
-// @version      1.8.3
-// @description  Multiplayer Script For Agar.io
+// @name        AgarioMods evergreen script
+// @namespace	 AgarioMods
+// @version      1.9.6
+// @description  community run mod feature set for agar.io
 // @author       electronoob
-// @match        http://agar.io/
+// @match        http*://agar.io/
 // @grant        none
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.src = "http://agariomods.com/mods.js";
+script.src = document.location.protocol+"//agariomods.com/mods.js";
 (document.body || document.head || document.documentElement).appendChild(script);
 
 /*
 repo:
 https://github.com/electronoob/agarmods
 
-common website for all mods from anybody:
 http://www.agariomods.com
 
 */
